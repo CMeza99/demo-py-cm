3a680a8baa7833251deb6537503928dd7700ae7e
Carlos Meza <carlos@digitalr00ts.com>
2019-05-20 22:06:24 -0700

    initial commit

---
+++ src/cm/__init__.py
@@ -0,0 +1,10 @@
+""" Package {{ cookiecutter.project_name }} top level """
+import logging
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    __version__ = None
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
